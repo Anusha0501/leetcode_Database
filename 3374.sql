6692daac9d641529e7c1f0de15fd75a5ca377274
Anusha Singh <anusha.22cse@bmu.edu.in>
2025-01-28 19:00:36 +0530

    Update 3374.sql

--- 3374.sql
+++ 3374.sql
@@ -1,23 +1,41 @@
 #3374. First Letter Capitalization II
 
-WITH SplitWords AS (
+WITH RECURSIVE SplitWords AS (
     SELECT
         content_id,
         content_text,
-        REGEXP_SPLIT_TO_TABLE(content_text, ' ') AS word
+        TRIM(SUBSTRING_INDEX(content_text, ' ', 1)) AS word,
+        TRIM(SUBSTRING(content_text, LENGTH(SUBSTRING_INDEX(content_text, ' ', 1)) + 2)) AS remaining_text,
+        1 AS word_order
     FROM
         user_content
+    UNION ALL
+    SELECT
+        content_id,
+        content_text,
+        TRIM(SUBSTRING_INDEX(remaining_text, ' ', 1)) AS word,
+        TRIM(SUBSTRING(remaining_text, LENGTH(SUBSTRING_INDEX(remaining_text, ' ', 1)) + 2)) AS remaining_text,
+        word_order + 1
+    FROM
+        SplitWords
+    WHERE
+        remaining_text != ''
 ),
 CapitalizedWords AS (
     SELECT
         content_id,
         content_text,
         word,
+        word_order,
         CASE
             WHEN word LIKE '%-%' THEN
-                INITCAP(SPLIT_PART(word, '-', 1)) || '-' || INITCAP(SPLIT_PART(word, '-', 2))
+                CONCAT(
+                    CONCAT(UPPER(SUBSTRING(SUBSTRING_INDEX(word, '-', 1), 1, 1)), LOWER(SUBSTRING(SUBSTRING_INDEX(word, '-', 1), 2))),
+                    '-',
+                    CONCAT(UPPER(SUBSTRING(SUBSTRING_INDEX(word, '-', -1), 1, 1)), LOWER(SUBSTRING(SUBSTRING_INDEX(word, '-', -1), 2)))
+                )
             ELSE
-                INITCAP(word)
+                CONCAT(UPPER(SUBSTRING(word, 1, 1)), LOWER(SUBSTRING(word, 2)))
         END AS capitalized_word
     FROM
         SplitWords
@@ -26,7 +44,7 @@ ReconstructedText AS (
     SELECT
         content_id,
         content_text,
-        STRING_AGG(capitalized_word, ' ' ORDER BY ordinality) AS converted_text
+        GROUP_CONCAT(capitalized_word ORDER BY word_order SEPARATOR ' ') AS converted_text
     FROM
         CapitalizedWords
     GROUP BY
@@ -40,4 +58,3 @@ FROM
     ReconstructedText
 ORDER BY
     content_id;
-
