c30561654c6c02990fb8e956bfd38cf7a0416ab3
Anusha Singh <anusha.22cse@bmu.edu.in>
2025-01-28 17:11:52 +0530

    Create 607.sql

---
+++ 607.sql
@@ -0,0 +1,10 @@
+#607. Sales Person
+
+select s.name
+from salesPerson as s
+where s.sales_id not in
+(select o.sales_id from orders as o
+left join company as c
+on o.com_id=c.com_id
+where name="RED")
+
